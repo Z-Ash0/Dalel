e4ccd1bb1c6585aa29651f6897dc26e50d3a0105
Z-Ash0 <zfire3005@gmail.com>
2024-12-04 21:33:20 +0200

    new changes

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesMosque
+  /// assets/images/mosque.svg
+  static const String assetsImagesMosque = "assets/images/mosque.svg";
+
+  /// Assets for assetsImagesOnBoarding1
+  /// assets/images/onBoarding1.png
+  static const String assetsImagesOnBoarding1 = "assets/images/onBoarding1.png";
+
+  /// Assets for assetsImagesOnBoarding2
+  /// assets/images/onBoarding2.png
+  static const String assetsImagesOnBoarding2 = "assets/images/onBoarding2.png";
+
+  /// Assets for assetsImagesOnBoarding3
+  /// assets/images/onBoarding3.png
+  static const String assetsImagesOnBoarding3 = "assets/images/onBoarding3.png";
+
+  /// Assets for assetsImagesPyramids
+  /// assets/images/pyramids.svg
+  static const String assetsImagesPyramids = "assets/images/pyramids.svg";
+
+  /// Assets for assetsImagesSuccessOrder
+  /// assets/images/successOrder.png
+  static const String assetsImagesSuccessOrder = "assets/images/successOrder.png";
+}
+
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xffC49E85);
+  static const deepBrown = Color(0xff6B4B3E);
+  static const lightGrey = Color(0xffB4B4B4);
+  static const grey = Color(0xff999999);
+  static const offWhite = Color(0xfff8f4f9);
+  static const black = Color(0xff333333);
+}
